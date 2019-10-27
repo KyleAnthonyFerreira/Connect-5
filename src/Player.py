639caae7a6454e1e03f1fcb497152b96b4021bad
Shae <shaebsimeoni@gmail.com>
2019-10-27 16:02:21 -0400

    Combining Human.py and AI.py into the same file as Player.py for simplicity

--- src/Player.py
+++ src/Player.py
@@ -16,3 +16,32 @@ class Player:
         """
         pass
 
+
+class Human(Player):
+
+    def __init__(self, name: str, board: Board):
+        Player.__init__(self, name, board)
+
+    def make_a_move(self, x, y):
+        """
+        Takes user input to make a move.
+        :param x:
+        :param y:
+        :return: None
+        """
+        pass
+
+
+class EasyAI(Player):
+
+    def __init__(self, name: str, board: Board):
+        Player.__init__(self, name, board)
+
+    def make_a_move(self, x, y):
+        """
+        Follows "Random" algorithm to make a move.
+        :param x:
+        :param y:
+        :return: None
+        """
+        pass
