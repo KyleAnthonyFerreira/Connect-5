8fb53a8f9a2a79dec9da315a024ba322d59ed136
Joshua Nelson <joshua.nelson@mail.utoronto.ca>
2019-12-04 14:30:38 -0500

    Added documentation and changed variable names for readability

--- src/Board.py
+++ src/Board.py
@@ -96,6 +96,12 @@ class Board:
             return -1, -1
 
     def has_connect5(self, player1, player2):
+        """
+        Will determine if any players have 5 pieces (or more) connected in a line
+        :param player1:
+        :param player2:
+        :return: player1/2.name or ""
+        """
         if self._connect_n(player1) >= 5:
             return player1.name
         elif self._connect_n(player2) >= 5:
@@ -105,8 +111,9 @@ class Board:
 
     def _connect_n(self, player) -> int:
         """
+        Will return the greatest number of pieces in a line for a specific player
         :param player:
-        :return: bool
+        :return: int
         """
         max_value = 0
         for i in range(self.dimension):
@@ -117,88 +124,111 @@ class Board:
                         max_value = temp
         return max_value
 
-    def sum_in_line(self, player, i, j, a, b, is_inverted) -> (int, int, int):
+    def sum_in_line(self, player, row, col, d1, d2, is_player1) -> (int, int, int):
+        """
+        Used in some AI's, this function will determine how many pieces are connected in a line. This function will
+        count for player 1 if is_player1 is true. Otherwise, this function will count for player2
+        :param player:
+        :param row:
+        :param col:
+        :param d1:
+        :param d2:
+        :param is_player1:
+        :return:
+        """
+        # Used to scale the directions
         x = 0
         y = 0
         sum = 0
 
-        if a == 0 and b == 0:
+        if d1 == 0 and d2 == 0:
             return -1, -1, -1
 
         while True:
-            if not is_inverted:
-                if self.is_valid(i + x, j + y):
-                    if self.get_piece(i + x, j + y) == player:
+            if not is_player1:
+                if self.is_valid(row + x, col + y):
+                    if self.get_piece(row + x, col + y) == player:
                         sum += 1
-                    elif self.is_empty(i + x, j + y):
-                        return int(i + x), int(j + y), int(sum)
+                    elif self.is_empty(row + x, col + y):
+                        return int(row + x), int(col + y), int(sum)
                     else:
                         break
                 else:
                     return -1, -1, -1
-                x = x + a
-                y = y + b
+                x = x + d1
+                y = y + d2
             else:
-                if self.is_valid(i + x, j + y):
-                    if self.is_empty(i + x, j + y):
-                        return int(i + x), int(j + y), int(sum)
-                    elif self.get_piece(i + x, j + y) != player and not self.is_empty(i + x, j + y):
+                if self.is_valid(row + x, col + y):
+                    if self.is_empty(row + x, col + y):
+                        return int(row + x), int(col + y), int(sum)
+                    elif self.get_piece(row + x, col + y) != player and not self.is_empty(row + x, col + y):
                         sum += 1
                     else:
                         break
                 else:
                     return -1, -1, -1
-                x = x + a
-                y = y + b
+                x = x + d1
+                y = y + d2
         return -1, -1, -1
 
-    def connect_x(self, player, i, j) -> int:
+    def connect_x(self, player, row, col) -> int:
         """
-                @TODO Make Look Nice :)
-                :param player:
-                :param i:
-                :param j:
-                :return: bool
-                """
+        :param player:
+        :param row:
+        :param col:
+        :return: bool
+        """
         max_value = 0
-        for a in range(-1, 2):
+        for d1 in range(-1, 2):
+            # Used to scale the directions
             c = 1
             sum = 1
-            b = -1
-            while b < 2:
-                if self.is_valid(i + (a * c), j + (b * c)):
-                    if self.get_piece(i + (a * c),
-                                      j + (b * c)) == player.name and not (
-                            a == 0 and b == 0):
+            d2 = -1
+            while d2 < 2:
+                if self.is_valid(row + (d1 * c), col + (d2 * c)):
+                    if self.get_piece(row + (d1 * c),
+                                      col + (d2 * c)) == player.name and not (
+                            d1 == 0 and d2 == 0):
                         sum += 1
                     else:
                         c = 0
                         sum = 1
-                        b += 1
+                        d2 += 1
                 else:
                     c = 0
                     sum = 1
-                    b += 1
+                    d2 += 1
                 c += 1
                 if sum >= max_value:
                     max_value = sum
         return max_value
 
-    def other_direction(self, player, i, j, a, b, is_inverted) -> int:
-        if a == 0 and b == 0:
+    def other_direction(self, player, row, col, d1, d2, is_player1) -> int:
+        """
+        A function used by some AI's, used to calculate the number of pieces in a line at a given position in a given
+        direction
+        :param player:
+        :param row:
+        :param col:
+        :param d1:
+        :param d2:
+        :param is_player1:
+        :return: number of pieces that are in a line
+        """
+        if d1 == 0 and d2 == 0:
             return 0
         sum = 0
         c = 1
         while True:
-            if self.is_valid(i+(a*c), j+(b*c)):
-                if not is_inverted:
-                    if self.get_piece(i+(a*c), j+(b*c)) == player:
+            if self.is_valid(row+(d1*c), col+(d2*c)):
+                if not is_player1:
+                    if self.get_piece(row+(d1*c), col+(d2*c)) == player:
                         sum += 1
                     else:
                         return sum
                     c += 1
                 else:
-                    if self.get_piece(i+(a*c), j+(b*c)) != player and not self.is_empty(i+(a*c), j+(b*c)):
+                    if self.get_piece(row+(d1*c), col+(d2*c)) != player and not self.is_empty(row+(d1*c), col+(d2*c)):
                         sum += 1
                     else:
                         return sum
